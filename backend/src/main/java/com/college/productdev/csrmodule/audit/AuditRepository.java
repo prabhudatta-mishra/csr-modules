df610a0507c606f717ad8c4f7346be7fe3edaed6
prabhudatta-mishra <prabhudattamishra260@gmail.com>
2025-11-03 21:47:28 +0530

    add component

--- backend/src/main/java/com/college/productdev/csrmodule/audit/AuditRepository.java
+++
@@ -1,8 +0,0 @@
-package com.college.productdev.csrmodule.audit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AuditRepository extends JpaRepository<AuditEvent, Long> {
-    List<AuditEvent> findTop200ByUserEmailOrderByOccurredAtDesc(String userEmail);
-}
